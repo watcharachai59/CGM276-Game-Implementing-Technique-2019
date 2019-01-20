79d60643138df57115e718b9173a1f951e2d8f22
watcharachai59 <46855973+watcharachai59@users.noreply.github.com>
2019-01-20 11:52:13 +0700

    UDP Client Server

---
+++ 01 intro/UDPClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String server = "10.1.116.239"; // loop back address
+            int servPort = 5000;
+
+            String message = "watcharachai-5903622";
+
+            byte[] sendPacket = Encoding.ASCII.GetBytes(message);
+
+            UdpClient client = new UdpClient();
+
+            try{
+                client.Send(sendPacket,sendPacket.Length,server,servPort);
+            }
+            catch(SocketException e)
+            {
+                Console.WriteLine(e.ErrorCode+" "+e.Message);
+            }
+        }
+    }
+}
